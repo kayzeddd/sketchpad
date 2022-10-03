92871e9a329d976822eeb7ebdaa4f8fd7bbff602
kayzed <kayzed98@gmail.com>
2022-10-03 18:12:13 -0400

    Add hover color change and grid size prompt
    All working well
    100 max square per side

--- jscript.js
+++ jscript.js
@@ -1,12 +1,13 @@
 const body = document.querySelector("body");
-
+body.classList.add("bodyItems")
 //container
 const container= document.createElement("div");
+container.classList.add("containerBorder")
 body.appendChild(container);
 
 //grid
 function containerSize(columns,columnSize,rows,rowSize){
-container.style.cssText = `display:grid; 
+container.style.cssText = `
                            grid-template-columns: repeat(${columns}, ${columnSize}px);
                            grid-template-rows: repeat(${rows}, ${rowSize}px);`
 createDivs(columns*rows)
@@ -17,16 +18,41 @@ function createDivs(n){
     const boxes =[];
     for (let i= 0; i < n; i++){
         boxes[i] = document.createElement("div");
+        //boxes[i].classList.add("boxBorder");
     }
+    boxes.forEach(box => box.addEventListener("mouseover", (e) => color(e)));
     appendDivs(boxes);
 }
 
+//hover
+function color(e){
+    let box = e.target;
+    box.classList.add("bgColorChange"); 
+    }
+
+//
 function appendDivs(array){
     for (let i =0; i < array.length; i++){
-        array[i].classList.add("boxBorder");
         container.appendChild(array[i]);
     }
 }
 
-//
-containerSize(20,50,20,50)
+//prompt button
+const btnDiv = document.createElement("div");
+btnDiv.classList.add("buttonDiv")
+const askUserBtn = document.createElement("button");
+askUserBtn.textContent = "Change Grid Size";
+btnDiv.appendChild(askUserBtn) ;
+body.insertBefore(btnDiv, container) ;
+
+btnDiv.addEventListener('click', (e) => userPrompt());
+
+function userPrompt() {
+    let askUser = prompt("How many squares per side?", 50);
+    if (askUser <= 100){
+        containerSize(askUser, 10, askUser, 10);
+    }
+    else {
+        alert("bro I will crash")
+    }
+}
